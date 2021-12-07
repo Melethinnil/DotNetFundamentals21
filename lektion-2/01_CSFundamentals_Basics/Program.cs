ef87ff64ce9f62b0b8b8b056469804f43eb51a8d
Amanda <95625417+Melethinnil@users.noreply.github.com>
2021-12-07 12:05:30 +0100

    Initial commit

---
+++ lektion-2/01_CSFundamentals_Basics/Program.cs
@@ -0,0 +1,52 @@
+﻿#region Comments
+
+//  Inline comment - only spans the one line
+
+/*  Block comment(ctrl + shift + *)
+ *      Can span multiple lines
+*/
+
+/// <summary>
+/// Summary comment for methods, to show information about its arguments in IntelliSense
+/// </summary>
+
+#endregion
+
+#region Data Types
+
+/*
+ * FRONTEND (REACT JS) - BACKEND API (C#) - DATABASE (SQL)
+ * 
+ * JS               C#                                                      SQL
+ * ------------------------------------------------------------------------------------------------------------------------
+ * Number           sbyte, byte, ushort, int, uint, long, ulong,            bit, tinyint, smallint, int, bigint, decimal,
+ *                  Int16, Int32, Int64, decimal, double, float             float, real, smallmoney, money, numeric
+ * 
+ * String           char, string                                            char, varchar, text, nchar, nvarchar, ntext
+ * Boolean          bool                                                    bit
+ * Object           object                                                  ---
+ * null (Object)    null                                                    null
+ * Undefined        null                                                    null
+ * ---              dynamic                                                 ---
+ * ---              custom                                                  ---
+ * ---              Guid (Global Unique Identifier)                         uniqueidentifier
+ * 
+ * 
+ * C# Integrals - någon form av nummer
+ * TYPE             BIT             MIN VALUE                          MAX VALUE
+ * ------------------------------------------------------------------------------------------------------------------------
+ * sbyte            8               -128                            127
+ * byte             8               0                               255
+ * short            16              -32,768                         32,767
+ * ushort           16              0                               65,535
+ * int              32              -2,147,483,648                  2,147,485,647
+ * uint             32              0                               4,292,967,295
+ * long             64              -9,223,372,036,854,775,808      9,223,372,036,854,775,807
+ * ulong            64              0                               18,446,744,073,709,551,615
+ * 
+ * Int16            16              same as short
+ * Int32            32              same as int
+ * Int64            64              same as long
+ */
+
+#endregion
