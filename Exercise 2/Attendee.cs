3d720c3acb0425bf12d05499c874aced3e97032c
Amanda <95625417+Melethinnil@users.noreply.github.com>
2021-12-08 12:14:06 +0100

    Completed Exercise 2

---
+++ Exercise 2/Attendee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2
+{
+    internal class Attendee
+    {
+        public string firstName;
+        public string lastName;
+        public string emailAdress;
+        public int age;
+        public string allergies;
+
+        public Attendee(string firstName, string lastName, string emailAdress, int age, string allergies)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAdress = emailAdress;
+            this.age = age;
+            this.allergies = allergies;
+        }
+    }
+}
